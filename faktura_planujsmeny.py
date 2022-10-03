c05077b0b776f8035d4646603c325178bf475180
Jaroslav Vážný <vazny@gaussalgo.com>
2022-10-03 14:35:12 +0200

    bugs

--- faktura_planujsmeny.py
+++ faktura_planujsmeny.py
@@ -9,7 +9,6 @@ import datetime as dt
 import pandas as pd
 from dateutil.parser import  parse
 
-fn = 'smeny_2022-06-01_Jaroslav-Vážný.xls'
 data = pd.read_html(fn, decimal=",")[0].iloc[:-2]
 data.columns = data.columns.get_level_values(1)
 data =  data.loc[:, ~data.columns.duplicated()]
@@ -20,5 +19,4 @@ data['Datum vystoupení'] = data['date']
 data['Typ vystoupení / specifikace'] = '3 ceremoniály'
 data['Čas trvání min.'] = (data['Délka'].astype(int)/100*60).astype(int)
 data['Jméno klienta'] = 'Wellness resort s.r.o.'
-
-data.iloc[:,6:].to_csv(f"{fn.rsplit('.', 1)[0]}.csv", index=False, header=True)
+data.iloc[:,4:].to_csv(f"{fn.rsplit('.', 1)[0]}.csv", index=False, header=True)
